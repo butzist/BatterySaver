34fc76dac8322fc3a1bc0c9143fafbe481c5dec4
Adam M. Szalkowski <adam.szalkowski@inf.ethz.ch>
2013-07-02 13:43:51 +0200

    autostart after upgrade or installation (not tested)

--- src/de/szalkowski/adamsbatterysaver/AutoStartReceiver.java
+++ src/de/szalkowski/adamsbatterysaver/AutoStartReceiver.java
@@ -10,7 +10,9 @@ public class AutoStartReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+		if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
+				intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED) ||
+				intent.getAction().equals(Intent.ACTION_PACKAGE_REPLACED)) {
 			Intent service = new Intent(context, de.szalkowski.adamsbatterysaver.MainService.class);
 	        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
 			if(settings.getBoolean(MainActivity.SETTINGS_START_SERVICE, true)) {
