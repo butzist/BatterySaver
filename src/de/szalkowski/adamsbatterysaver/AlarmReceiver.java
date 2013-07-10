5c3a5eff7e5915eb019b824646acf798ac6ec679
Adam M. Szalkowski <adam.szalkowski@inf.ethz.ch>
2013-07-10 10:22:45 +0200

    fixed hardware requirements and NullPointerException in AlarmReceiver

--- src/de/szalkowski/adamsbatterysaver/AlarmReceiver.java
+++ src/de/szalkowski/adamsbatterysaver/AlarmReceiver.java
@@ -4,17 +4,30 @@ import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
-
+	static private final String LOG = "de.szalkowski.adamsbatterysaver.AlarmReceiver";
+	
 	@SuppressLint("Wakelock")
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if(!MainService.wake_lock.isHeld()) {
+		Intent service = new Intent(context,MainService.class);
+
+		if(!MainService.is_running) {
+			Log.e(LOG, "unexpected timer event - service terminated?");
+	        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+			if(settings.getBoolean(MainActivity.SETTINGS_START_SERVICE, true)) {
+				context.startService(service);
+			}
+		}
+		
+		if(MainService.wake_lock != null && !MainService.wake_lock.isHeld()) {
 			MainService.wake_lock.acquire();
 		}
 
-		Intent service = new Intent(context,MainService.class);
 		if(intent.getAction().equals(MainService.ACTION_WAKEUP_TIMEOUT)) {
 			service.setAction(MainService.ACTION_WAKEUP_TIMEOUT);
 		} else if(intent.getAction().equals(MainService.ACTION_POWER_TIMEOUT)) {
